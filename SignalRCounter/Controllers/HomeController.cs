c8ec9cb3f1300b63ff6d3134870ee7aabe64348b
Natanael Zimmer <nrzimmer@gmail.com>
2019-06-25 03:22:40 -0300

    Adicionado projeto com um contador simples usando SignalR e um HUB
    Código baseado nisso
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?tabs=visual-studio&view=aspnetcore-2.2
    
    O Timer foi criado no Program para que não seja criado um novo a cada
    request para a pagina /home
    
    A cada request a essa pagina ele seta um novo handler do timer, e por
    consequencia zera o contador
    
    Se o contador tiver que continuar entre diferentes usuarios, ele teria
    que ter seu valor guardado em uma instancia constante, tipo o program
    que foi usado para criar a variavel aTimer

---
+++ SignalRCounter/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRCounter.Hubs;
+using SignalRCounter.Models;
+
+namespace SignalRCounter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int aNumber = 0;
+
+        public HomeController(IHubContext<CounterHub> hubcontext)
+        {
+            //Create do controller
+            //Pega o CounterHub
+            HubContext = hubcontext;
+            //Cria um novo handler para o evento de timer e remove o antigo se houver
+            System.Timers.ElapsedEventHandler aHandler = (sender, e) => { this.HubContext.Clients.All.SendAsync("ReceiveMessage", aNumber.ToString()); aNumber++; };
+            Program.aTimer.Elapsed -= Program.aLastHandler;
+            Program.aTimer.Elapsed += aHandler;
+            Program.aLastHandler = aHandler;
+        }
+
+        private IHubContext<CounterHub> HubContext
+        {
+            get;
+            set;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ SignalRCounter/Hubs/CounterHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRCounter.Hubs
+{
+    public class CounterHub : Hub
+    {
+        public async Task SendMessage(int number)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", number.ToString());
+        }
+    }
+}
